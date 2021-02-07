c7cbaeb38f23a88c613171e8f5c78d5e5e7934be
AmandaCacholi <amanda.cacholi@outlook.com>
2021-02-07 20:12:42 -0300

    atualização

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const bodyparser = require('body-parser')
 const routes = require('./router')
 const Usuario = require('./models/usuario');
 
-
 const porta = 3000;
 
 app.use(bodyparser.json())
 routes(app)
-
-
 
 app.listen(porta, () => console.log(`[Informação]: Servidor rodando na porta: ${porta}`))
